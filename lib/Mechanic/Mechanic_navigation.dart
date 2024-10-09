9a54183125f0e8450cb910f3f9293661a32045eb
jibindaskv <jibindas.work@gmail.com>
2024-10-09 10:58:32 -0700

    first

---
+++ lib/Mechanic/Mechanic_navigation.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'Mechanic_rating.dart';
+import 'Mechanic_service.dart';
+import 'Mechanic_service_and_request.dart';
+
+class Mechanic_navigation extends StatefulWidget {
+  Mechanic_navigation({Key? key}) : super(key: key);
+
+  @override
+  _Mechanic_navigationState createState() => _Mechanic_navigationState();
+}
+
+class _Mechanic_navigationState extends State<Mechanic_navigation> {
+  int _selectedIndex = 0;
+
+  static List<dynamic> _widgetOptions = [
+    Mechanic_service_and_request(),
+    Mechanic_service(),
+    Mechanic_rating(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      initialIndex: 0,
+      child: Scaffold(
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.person_search_outlined,
+                    size: 30,
+                  ),
+                  label: 'Request',
+                  backgroundColor: Colors.white),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home_repair_service,
+                    size: 30,
+                  ),
+                  label: 'Service',
+                  backgroundColor: Colors.white),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    CupertinoIcons.star_fill,
+                    size: 32,
+                  ),
+                  label: 'Rating',
+                  backgroundColor: Colors.white),
+            ],
+            type: BottomNavigationBarType.shifting,
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.blue,
+            showUnselectedLabels: true,
+            unselectedItemColor: Colors.black,
+            selectedIconTheme: IconThemeData(color: Colors.blue),
+            iconSize: 40,
+            onTap: _onItemTapped,
+            elevation: 5),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:brakedown_assist/Home.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'firebase_options.dart';
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+// ...
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      builder: (context, child) => MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Home(),
+      ),
+        designSize: Size(448, 998)
+    );
+  }
+}
